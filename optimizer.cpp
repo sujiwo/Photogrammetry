cacc5f67e98d9a661f7599501452b7060a238a1c
Adi Sujiwo <sujiwo@gmail.com>
2018-06-26 15:14:06 +0900

    Compile g2o

---
+++ optimizer.cpp
@@ -0,0 +1,19 @@
+/*
+ * optimizer.cpp
+ *
+ *  Created on: Jun 26, 2018
+ *      Author: sujiwo
+ */
+
+
+#include "optimizer.h"
+#include "g2o/core/block_solver.h"
+
+
+using namespace Eigen;
+
+
+void bundle_adjustment ()
+{
+
+}
---
+++ optimizer.h
@@ -0,0 +1,16 @@
+/*
+ * optimizer.h
+ *
+ *  Created on: Jun 26, 2018
+ *      Author: sujiwo
+ */
+
+#ifndef OPTIMIZER_H_
+#define OPTIMIZER_H_
+
+
+#include "KeyFrame.h"
+#include "MapPoint.h"
+
+
+#endif /* OPTIMIZER_H_ */
