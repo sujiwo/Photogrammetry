a4afb40ace7df4b15b7175cdb5c8c87196ec1d82
Adi Sujiwo <sujiwo@gmail.com>
2018-06-27 11:49:40 +0900

    New params

--- optimizer.cpp
+++ optimizer.cpp
@@ -8,12 +8,14 @@
 
 #include "optimizer.h"
 #include "g2o/core/block_solver.h"
+#include "g2o/core/sparse_optimizer.h"
 
 
+using namespace std;
 using namespace Eigen;
 
 
-void bundle_adjustment ()
+void bundle_adjustment (vector<KeyFrame*> &kfList, vector<MapPoint*> &mpList)
 {
 
 }
